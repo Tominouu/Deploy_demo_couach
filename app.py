681d242e28c8d18395b55f19836b69ff806197d9
Tom <t.leclercq@couach.com>
2025-07-03 12:13:20 +0000

    app route for error 404

--- app.py
+++ app.py
@@ -68,6 +68,10 @@ def register():
 def test():
         return render_template('404.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ =='__main__':
     init_db()
     app.run(host='0.0.0.0', port=8080)
