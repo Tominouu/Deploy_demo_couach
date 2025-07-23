73d79192e33e7416ac47f74c8a5065c5d376190e
Tom <t.leclercq@couach.com>
2025-07-23 11:48:27 +0200

    add all

--- app.py
+++ app.py
@@ -1244,4 +1244,4 @@ def test(room_id):
 if __name__ =='__main__':
     print(datetime.datetime.now(ZoneInfo("Europe/Paris")))
     init_db()
-    app.run(host='0.0.0.0', port=8080, ssl_context=('cert.pem', 'key.pem'))  # Utiliser SSL pour la sécurité
+    app.run(host='0.0.0.0', port=8080)  # Utiliser SSL pour la sécurité
